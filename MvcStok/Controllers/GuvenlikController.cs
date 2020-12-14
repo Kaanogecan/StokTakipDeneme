63aae46c5d6e245eecdaba8097951965e752e2e5
Kaanogecan <kaanogecan@gmail.com>
2020-12-14 16:08:05 +0300

    first commit

---
+++ MvcStok/Controllers/GuvenlikController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class GuvenlikController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Guvenlik
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLKULLANICI t)
+        {
+
+            var bilgiler = db.TBLKULLANICI.FirstOrDefault(x => x.AD == t.AD && x.SIFRE == t.SIFRE);
+            if (bilgiler != null)
+            {
+               return RedirectToAction("Index", "Kategori");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+    }
+}
